adf65b3f37db1d5e89e99140aaff9f1002b7aba7
EUIBIN_BAE <ebbae@suresofttech.com>
2023-09-20 08:41:44 +0900

    Remove t option

--- jarvis/action.sh
+++ jarvis/action.sh
@@ -13,7 +13,7 @@ echo "[DEBUG] GITHUB_TOKEN: $TOKEN"
 
 docker build -t ubuntu20.04 $GITHUB_ACTION_PATH
 
-docker run -d --name jarvis-ubuntu20.04 -u jarvis:jarvis -it ubuntu20.04
+docker run -d --name jarvis-ubuntu20.04 -u jarvis:jarvis -i ubuntu20.04
 
 retval=$?
 if [ $retval -ne 0 ]; then
@@ -35,7 +35,7 @@ echo "mkdir test"
 
 docker cp "$CSBUILD_PATH" jarvis-ubuntu20.04:$JARVIS_WORKSPACE/tbeg/
 docker cp "$OPENAI_PATH" jarvis-ubuntu20.04:$JARVIS_WORKSPACE/openai/
-docker cp "$GITHUB_ACTION_PATH/jarvis/env_sn/git_config.sh" jarvis-ubuntu20.04:$JARVIS_WORKSPACE/scripts/
+docker cp "$GITHUB_ACTION_PATH/jarvis/env_sh/git_config.sh" jarvis-ubuntu20.04:$JARVIS_WORKSPACE/scripts/
 
 docker exec -itu 0 jarvis-ubuntu20.04 sh -c "$JARVIS_WORKSPACE/scripts/git_config.sh"
 docker exec -itu 0 jarvis-ubuntu20.04 sh -c "$JARVIS_WORKSPACE/scripts/setenv_docker.sh"
