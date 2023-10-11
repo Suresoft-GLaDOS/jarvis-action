9434f730b0cfa8220cf13d38c6c1d57814942a42
EUIBIN_BAE <ebbae@suresofttech.com>
2023-10-11 16:44:59 +0900

    Use git apply command

--- jarvis/git/create-pull-request.py
+++ jarvis/git/create-pull-request.py
@@ -44,7 +44,7 @@ def run():
     now = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
     patch_branch = f"{GITHUB_REF_NAME}-auto-patch-{now}"
     os.system(f"git checkout -b {patch_branch}")
-    os.system(f"patch -p0 < {patch_path}")
+    os.system(f"git apply < {patch_path}")
     os.system(f"git add .")
     os.system(f"git commit -m \"Fixed automatically #{PR_INFO['issue_number']} by Vulcan\"")
     os.system(f"git push origin {patch_branch}")
