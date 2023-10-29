baf0756a862cdd40eccc3e3787318041b56824c1
EUIBIN_BAE <ebbae@suresofttech.com>
2023-10-30 08:45:24 +0900

    Make diff file in outputs

--- jarvis/git/diff_gen.sh
+++ jarvis/git/diff_gen.sh
@@ -5,5 +5,5 @@ echo $JARVIS_WORKSPACE/JARVIS/workspace/home/workspace/$TARGET_REPO_NAME
 
 mkdir patches
 
-git diff -- ':(exclude)*/.staticdata/*' > fix_vioation.patch
+git diff -- ':(exclude)*/.staticdata/*' > $JARVIS_WORKSPACE/JARVIS/workspace/outputs/fix_vioation.patch
 mv fix_violation.patch $JARVIS_WORKSPACE/JARVIS/workspace/outputs
