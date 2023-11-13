70b5380d481f99893d42b96baa12eab74098a426
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-13 15:14:50 +0900

    Set env vars with sh script

--- jarvis/action.sh
+++ jarvis/action.sh
@@ -74,9 +74,13 @@ echo "JARVIS clone"
 
 docker exec -iu 0 jarvis-ubuntu20.04 sh -c "cd JARVIS; git checkout action_check; git pull"
 
+# docker exec -iu 0 jarvis-ubuntu20.04 sh -c "pip install -r $JARVIS_WORKSPACE/JARVIS/requirements.txt;\
+#                                             export JARVIS_WORKSPACE=$JARVIS_WORKSPACE; \
+#                                             export JARVIS_TARGET=$JARVIS_TARGET; \
+#                                             python3 $JARVIS_WORKSPACE/JARVIS/main.py"
+
 docker exec -iu 0 jarvis-ubuntu20.04 sh -c "pip install -r $JARVIS_WORKSPACE/JARVIS/requirements.txt;\
-                                            export JARVIS_WORKSPACE=$JARVIS_WORKSPACE; \
-                                            export JARVIS_TARGET=$JARVIS_TARGET; \
+                                            $JARVIS_WORKSPACE/scripts/setenv_docker.sh \
                                             python3 $JARVIS_WORKSPACE/JARVIS/main.py"
 
 retval=$?
