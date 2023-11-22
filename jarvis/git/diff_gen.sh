3087bb400e41472abd2d7d13d8b9bba8b756b4b2
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-22 09:04:28 +0900

    Gen diff with path

--- jarvis/git/diff_gen.sh
+++ jarvis/git/diff_gen.sh
@@ -11,9 +11,11 @@ files=$(git diff --name-only)
 # 각 파일별로 diff를 생성합니다.
 for file in $files; do
     # 경로에서 슬래시를 대시로 대체하여 파일명을 생성
-    diff_file="$(echo $file | sed 's/\//_/g').diff"
+    # diff_file="$(echo $file | sed 's/\//_/g').diff"
+    mkdir -p "$file"
+    rm -rf "$file"
     # git diff 결과를 파일에 저장
-    git diff -- "$file" > "$JARVIS_WORKSPACE/JARVIS/workspace/outputs/$diff_file"
-    echo "Saved diff for $file to $JARVIS_WORKSPACE/JARVIS/workspace/outputs/$diff_file"
+    git diff -- "$file" > "$JARVIS_WORKSPACE/JARVIS/workspace/outputs/$file"
+    echo "Saved diff for $file to $JARVIS_WORKSPACE/JARVIS/workspace/outputs/$file"
 done
 
