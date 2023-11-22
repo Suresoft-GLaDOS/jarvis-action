8c929bef9c2db30e5b49a684a1da8658f22e9d95
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-22 17:10:40 +0900

    To debug

--- jarvis/git/create-pull-request-local.py
+++ jarvis/git/create-pull-request-local.py
@@ -32,7 +32,7 @@ def construct_pr_info():
 def _gen_diff_list():
     output_dir = ACTION_TEMP_DIR
     print(f"Output temp dir: {output_dir}")
-    diff_list=(glob.glob(f"{output_dir}/**/*.diff", recursive=True))
+    diff_list=glob.glob(f"{output_dir}/**/*.diff", recursive=True)
     print(diff_list)
 
     return diff_list
