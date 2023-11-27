20c5c1bc50c3bd4f2a3c530d2fd7bcb975266611
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-27 11:52:15 +0900

    Follow unix style newliene

--- jarvis/action.sh
+++ jarvis/action.sh
@@ -56,6 +56,8 @@ docker cp "$GITHUB_ACTION_PATH/jarvis/env_sh/setenv_docker.sh" jarvis-ubuntu20.0
 docker cp "$GITHUB_ACTION_PATH/jarvis/git/" jarvis-ubuntu20.04:$JARVIS_WORKSPACE/scripts/
 docker cp "$GITHUB_WORKSPACE" jarvis-ubuntu20.04:$JARVIS_WORKSPACE/
 
+docker exec -iu 0 jarvis-ubuntu20.04 sh -c "cd $JARVIS_WORKSPACE; git add ."
+
 export CSBUILD_DOCKER="$JARVIS_WORKSPACE/tbeg/apps/csbuild-ubuntu-20.04_v1.2.0/bin"
 
 docker exec -iu 0 jarvis-ubuntu20.04 sh -c "$JARVIS_WORKSPACE/scripts/setenv_docker.sh"
