3f1e164141c4729a92b1aecb17485d5224d87450
EUIBIN_BAE <ebbae@suresofttech.com>
2023-12-01 16:18:23 +0900

    Check GITHUB_WORKSPACE

--- jarvis/action.sh
+++ jarvis/action.sh
@@ -120,6 +120,7 @@ python3 -m pip install --upgrade pip
 pip install gitpython
 
 cd $GITHUB_WORKSPACE
+echo $GITHUB_WORKSPACE
 find . -type f -exec dos2unix {} \;
 
 python3 $GITHUB_ACTION_PATH/jarvis/git/create-pull-request-local.py                                            
