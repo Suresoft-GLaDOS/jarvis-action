ebfa2883737451e5639d931989c3f41dc169fa14
EUIBIN_BAE <ebbae@suresofttech.com>
2024-01-25 13:41:26 +0900

    Write token.txt too

--- jarvis/action.sh
+++ jarvis/action.sh
@@ -14,7 +14,7 @@ echo "[DEBUG] GITHUB_ACTION_PATH: $GITHUB_ACTION_PATH"
 echo "[DEBUG] JARVIS_WORKSPACE: $JARVIS_WORKSPACE"
 echo "[DEBUG] GITHUB_TOKEN: $TOKEN"
 echo $GITHUB_ACTION_PATH/token.txt
-echo $TOKEN > token.txt
+echo $TOKEN > $GITHUB_ACTION_PATH/token.txt
 echo "export TOKEN=$TOKEN; git clone https://$TOKEN@github.com/$GITHUB_REPOSITORY.git" > $GITHUB_ACTION_PATH/token.sh
 
 echo "[DEBUG] OPENAI_PATH: $OPENAI_PATH"
